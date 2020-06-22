9aa330f32b7f98bcaedb2ba7bbc8391a4b9d8e86
germanespinosa <germanespinosa@gmail.com>
2020-06-22 14:02:17 -0500

    none

--- scripts/install_core.sh
+++ scripts/install_core.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #curl https://raw.githubusercontent.com/germanespinosa/moukey/master/scripts/install_core.sh | sudo bash
 
-sudo apt-get install cmake git -y
+sudo apt-get install cmake git libevdev-dev -y
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 cd $tmp_dir
 git clone https://github.com/germanespinosa/moukey.git
